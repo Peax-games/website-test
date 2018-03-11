99be40371db5b9724c0c9c69d6390c2c4457cdd9
Christopher Fox <cfox1290@gmail.com>
2018-03-10 21:26:22 -0600

    added a video slider component for the main page

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,74 +1,67 @@
 import React, { Component } from 'react';
 import './Landing.css';
 import Chat from '../Chat/Chat';
-import Pong from './../../Pictures/ponggamejpeg.jpg';
-import Orb from './../../Pictures/cyberorbjpeg.jpg';
-import Invaders from './../../Pictures/invaders1jpeg.jpg';
-import Tanks from './../../Pictures/tanks1jpeg.jpg';
-import BrickBreakers from './../../Pictures/breakoutjpeg.jpg';
+
+import VideoSlider from './VideoSlider';
 
 
 class Landing extends Component {
     constructor() {
         super();
 
-        this.state = {
-            // sliderImages: ["url('./../../Pictures/checkers.jpeg')","url('./../../Pictures/black-jack.jpg')", "url('./../../Pictures/minesweeper.jpg')", "url('./../../Pictures/Screenshot (7).png')"],
-            sliderImages: [`url(${Pong}`, `url(${Orb}`, `url(${Invaders}`, `url(${BrickBreakers}`, `url(${Tanks}`],
-            cur: 0
-        }
-        this.slideLeft = this.slideLeft.bind(this);
-        this.slideRight = this.slideRight.bind(this);
+        // this.state = {
+        //     // sliderImages: ["url('./../../Pictures/checkers.jpeg')","url('./../../Pictures/black-jack.jpg')", "url('./../../Pictures/minesweeper.jpg')", "url('./../../Pictures/Screenshot (7).png')"],
+        //     sliderImages: [`url(${Pong}`, `url(${Orb}`, `url(${Invaders}`, `url(${BrickBreakers}`, `url(${Tanks}`],
+        //     cur: 0
+        // }
+        // this.slideLeft = this.slideLeft.bind(this);
+        // this.slideRight = this.slideRight.bind(this);
     }
 
-    slideLeft() {
-        console.log('left')
-        console.log(this.state.cur)
-        if (this.state.cur === 0) {
-            return this.setState({
-                cur: this.state.sliderImages.length - 1
-            })
-        } else {
-            return this.setState({
-                cur: this.state.cur - 1
-            })
-        }
-    }
-    slideRight() {
-        console.log('right')
-        console.log(this.state.cur)
-        if (this.state.cur >= this.state.sliderImages.length - 1) {
-            return this.setState({
-                cur: 0
-            })
-        } else {
-            return this.setState({
-                cur: this.state.cur + 1
-            })
-        }
-    }
+    // slideLeft() {
+    //     if (this.state.cur === 0) {
+    //         return this.setState({
+    //             cur: this.state.sliderImages.length - 1
+    //         })
+    //     } else {
+    //         return this.setState({
+    //             cur: this.state.cur - 1
+    //         })
+    //     }
+    // }
+    // slideRight() {
+    //     if (this.state.cur >= this.state.sliderImages.length - 1) {
+    //         return this.setState({
+    //             cur: 0
+    //         })
+    //     } else {
+    //         return this.setState({
+    //             cur: this.state.cur + 1
+    //         })
+    //     }
+    // }
 
     render() {
-        console.log('images here', this.state.sliderImages)
-        const sliderStyle = {
-            backgroundImage: this.state.sliderImages[this.state.cur]
-        }
-
+        // console.log('images here', this.state.sliderImages)
+        // const sliderStyle = {
+        //     backgroundImage: this.state.sliderImages[this.state.cur]
+        // }
+        // <h1 className="landing-logo"><p>P</p><p>E</p><p>A</p><p>X</p></h1>
+        // <div className="arrow-slider-container">
+        //     <div className="left-arrow" onClick={() => this.slideLeft()}></div>
+        //     <div className="game-slider-container">
+        //         <a href="genres">
+        //             <div className="game-slider" style={sliderStyle}></div>
+        //         </a>
+        //     </div>
+        //     <div className="right-arrow" onClick={() => this.slideRight()}></div>
+        // </div>
         return (
             <div className="landing_main_container">
-                <h1 className="landing-logo"><p>P</p><p>E</p><p>A</p><p>X</p></h1>
-                <div className="arrow-slider-container">
-                    <div className="left-arrow" onClick={() => this.slideLeft()}></div>
-                    <div className="game-slider-container">
-                        <a href="genres">
-                            <div className="game-slider" style={sliderStyle}></div>
-                        </a>
-                    </div>
-                    <div className="right-arrow" onClick={() => this.slideRight()}></div>
-                </div>
+                <VideoSlider />
                 <h3 className="global-chat-header">Discussion</h3>
                 <div className="landing-chat-container">
-                <Chat />
+                    <Chat />
                 </div>
                 <h3 className="game-list-header">Games</h3>
                 <div className="game-list-container" >
---
+++ src/components/Landing/VideoSlider.js
@@ -0,0 +1,93 @@
+import React, { Component } from 'react';
+
+export default class VideoSlider extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            slider: ['https://www.youtube.com/embed/IpqYfWKOZtE?rel=0&controls=0&showinfo=0&autoplay=1&loop=1&modestbranding=1', 'https://www.youtube.com/embed/CjustgpgebA?rel=0&controls=0&showinfo=0&autoplay=1&loop=1', 'https://www.youtube.com/embed/YNeMI78L9V0?rel=0&controls=0&showinfo=0&autoplay=1&loop=1'],
+            cur: 0,
+            dots: '',
+            vids: '',
+        }
+        this.slideLeft = this.slideLeft.bind(this);
+        this.slideRight = this.slideRight.bind(this);
+        this.currentSlide = this.currentSlide.bind(this);
+    }
+    componentDidMount() {
+
+        const CIRCLES = {
+            cursor: 'pointer',
+            height: '15px',
+            width: '15px',
+            margin: '2px 2px',
+            backgroundColor: '#bbb',
+            borderRadius: '50%',
+            display: 'inline-block',
+            textAlign: 'center',
+        };
+
+        const VIDSSLIDER = this.state.slider.map((x, i) => {
+            return (
+                <iframe key={i} height='600px' width='100%' src={x}></iframe>
+            )
+        })
+        const DOTS = this.state.slider.map((x, i) => {
+
+            return (
+                <span key={i} style={CIRCLES} onClick={() => this.currentSlide(i)}></span>
+            )
+        })
+        const intervalTime = setInterval(this.slideRight, 7000)
+        this.setState({ dots: DOTS, vids: VIDSSLIDER, intervalTime })
+    }
+    componentWillUnmount() {
+        return clearInterval(this.state.intervalTime);
+    }
+    slideLeft() {
+        if (this.state.cur === 0) {
+            return this.setState({
+                cur: this.state.vids.length - 1
+            })
+        } else {
+            return this.setState({
+                cur: this.state.cur - 1
+            })
+        }
+    }
+    slideRight() {
+        if (this.state.cur >= this.state.vids.length - 1) {
+            return this.setState({
+                cur: 0
+            })
+        } else {
+            return this.setState({
+                cur: this.state.cur + 1
+            })
+        }
+    }
+    currentSlide(x) {
+        this.setState({ cur: x })
+    }
+    render() {
+        const MAINWRAPPER = {
+            width: '100%',
+            height: '600px',
+
+        }
+        const CIRCLESDIV = {
+            textAlign: 'center',
+            width: '100%',
+            position: 'relative',
+        }
+        return (
+            <div style={MAINWRAPPER}>
+                <div>
+                    {this.state.vids[this.state.cur]}
+                </div>
+                <div style={CIRCLESDIV}>
+                    {this.state.dots}
+                </div>
+            </div>
+        )
+    }
+}
